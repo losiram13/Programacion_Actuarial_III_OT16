e93770978da39e0488619ade7d5066209cb24ebd
Marisol Vazquez <losiram_13_97@hotmail.com>
2016-09-22 13:43:42 -0500

    Caso I
    Función mediacontaminante
    Función completos
    Función corr

--- corr.R
+++ corr.R
@@ -1,28 +1,40 @@
-
-
 #Para obtener el coeficiente de correlación entre estas variables pueden ocupar la función cor().
 
 
 corr<-function(directorio,horizonte=0){
   
-  setwd("~/GitHub/Programacion_Actuarial_III_OT16/specdata")
-  resultado<-numeric()
-  completo<-integer()
-  for(n in 1:322){
+  setwd("C:/Users/marisol/Documents/GitHub/Programacion_Actuarial_III_OT16/specdata")
+  
+  completos<-function(directorio,id=1:332){
+    setwd("~/GitHub/Programacion_Actuarial_III_OT16/specdata")
+    completo<-integer()
+    for(n in id){
+      data1 <- read.csv(paste(formatC(n,width = 3, flag = "0"), ".Csv",sep = ""), header = T)
+      columnas <- data1[,2:3]
+      casoscompletos<-complete.cases(columnas)
+      j <- data1[,2:3][casoscompletos,]
+      
+      
+      completo<-c(completo,sum(complete.cases(data1[,2:3])))
+    }
+    table<-data.frame(Id=id,Nobs=completo)
+    table
+  }
+  
+  cd <- completos("specdata",1:332)
+  
+  
+  complet <- cd[cd["Nobs"] > horizonte,]$Id
+  resul <- numeric()
+  for(n in complet){
     data1 <- read.csv(paste(formatC(n,width = 3, flag = "0"), ".Csv",sep = ""), header = T)
     
+    comm <- data1[complete.cases(data1[,2:3]),]
     
-    completo<-c(sum(complete.cases(data1[,2:3])))
-    columnas <- data1[,2:3]
-    casoscompletos<-complete.cases(columnas)
-    j <- c(data1[,2:3][casoscompletos,])
-    if(completo>=horizonte)
-    {resultado<-c(resultado,cor(j$sulfate,j$nitrate))}
-    else
-    {resultado=0}
+    resul<-c(resul,cor(comm$sulfate,comm$nitrate))
+    
   }
-  resultado
+  resul
 }
+corr("specdata",18)
 
-
-corr("specdata",0)
--- mediacontaminante.R
+++ mediacontaminante.R
@@ -1,20 +1,19 @@
 
 
-
 mediacontaminante <- function(directorio,contaminante,id=1:322){
   setwd("~/GitHub/Programacion_Actuarial_III_OT16/specdata")
- datos<-numeric()
+  datos<-numeric()
   
-for(n in id){
-  data1 <- read.csv(paste(formatC(n,width = 3, flag = "0"), ".Csv",sep = ""), header = T)
-
-if(contaminante=="sulfate")
-  {r<-data1[,2]
-  datos<-c(datos,r)}
-  else
-  {n<-data1[,2]
+  for(n in id){
+    data1 <- read.csv(paste(formatC(n,width = 3, flag = "0"), ".Csv",sep = ""), header = T)
+    
+    if(contaminante=="sulfate")
+    {r<-data1[,2]
+    datos<-c(datos,r)}
+    else
+    {n<-data1[,3]
     datos<-c(datos,n)}
+  }
+  mean(datos,na.rm=TRUE)
 }
- mean(datos,na.rm=TRUE)
-}
-mediacontaminante("specdata","nitrate",1:10)
+mediacontaminante("specdata","nitrate",23)
