b385330d974eee1e46f64ec7a1e13882623ec8f7
Marisol Vazquez <losiram_13_97@hotmail.com>
2016-09-21 22:23:10 -0500

    Caso I
    Función Mediacontaminante
    Función Completos
    Función Corr

---
+++ Caso I/completos.R
@@ -0,0 +1,17 @@
+completos<-function(directorio,id=1:332){
+  setwd("~/GitHub/Programacion_Actuarial_III_OT16/specdata")
+  completo<-integer()
+  for(n in id){
+    data1 <- read.csv(paste(formatC(n,width = 3, flag = "0"), ".Csv",sep = ""), header = T)
+    columnas <- data1[,2:3]
+    casoscompletos<-complete.cases(columnas)
+    j <- data1[,2:3][casoscompletos,]
+    
+    
+    completo<-c(completo,sum(complete.cases(data1[,2:3])))
+  }
+  table<-data.frame(Id=id,Nobs=completo)
+  table
+}
+
+completos("specdata",1:322)
